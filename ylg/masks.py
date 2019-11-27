34c6e11ff89e4d6d2dd75b5bd7b521f4a9a42a96
Giannis Daras <daras.giannhs@gmail.com>
2019-11-27 19:33:12 +0200

    Implement masks from Sparse Transformers and FI extensions

--- ylg/masks.py
+++ ylg/masks.py
@@ -185,7 +185,247 @@ class SparseMask:
         return tensor
 
 
+class SubsequentMask(SparseMask):
+    @classmethod
+    @disallow_non_square
+    def get_indices(self, nL, **kwargs):
+        mask = self.get_mask(nL)
+        return tf.where(tf.equal(mask, 1))
 
+    @classmethod
+    @disallow_non_square
+    def get_mask(self, nL):
+        reverse = tf.convert_to_tensor(np.triu(np.ones((nL, nL)), k=1),
+                                       dtype=tf.float32)
+        return tf.dtypes.cast(tf.equal(reverse, 0), tf.float32)
+
+
+class LeftFloorMask(SparseMask):
+    @classmethod
+    @disallow_downsampling
+    @compute_stride
+    @numpy
+    def get_indices(self, nI, nO, stride=None, **kwargs):
+        indices = []
+        for row in range(nO):
+            lower = max(0, min(row - (row % stride), nI))
+            higher = min(row + 1, nI)
+            for col in range(lower, higher):
+                indices.append([row, col])
+        return indices
+
+
+class RightFloorMask(SparseMask):
+    @classmethod
+    @disallow_downsampling
+    @compute_stride
+    @numpy
+    def get_indices(self, nI, nO, stride=None, **kwargs):
+        indices = []
+        for row in range(nO):
+            lower = max(0, min(row - (row % stride), nI))
+            higher = min(row + 1, nI)
+            for col in range(lower, higher):
+                indices.append([col, row])
+        return indices
+
+
+class LeftRepetitiveMask(SparseMask):
+    @classmethod
+    @compute_stride
+    @allow_non_square
+    @numpy
+    def get_indices(self, nI, nO, stride=None, overlap=1, **kwargs):
+        indices = []
+        row_indices = np.arange(0, nI)
+        indxs = (row_indices % stride) >= (stride - overlap)
+        col_indices = row_indices[indxs]
+
+        for row in np.arange(0, nO):
+            if nI == nO:
+                indices.append([row, row])
+            for col in col_indices:
+                indices.append([row, col])
+        return indices
+
+
+class StridedMask1(SparseMask):
+    @classmethod
+    @disallow_non_square
+    @compute_stride
+    @numpy
+    def get_indices(self, nL, stride=None, **kwargs):
+        indices = []
+        for row in range(nL):
+            low = max(0, row - stride + 1)
+            for col in range(low, row + 1):
+                indices.append([row, col])
+        return indices
+
+
+class StridedMask2(SparseMask):
+    @classmethod
+    @disallow_non_square
+    @compute_stride
+    @numpy
+    def get_indices(self, nL, stride=None, **kwargs):
+        indices = []
+        for row in range(nL):
+            for col in range(nL):
+                if abs(row - col) % stride == 0:
+                    indices.append([row, col])
+        return indices
+
+
+class OneDirectionalRepetitiveMask(SparseMask):
+    @classmethod
+    def get_mask(self, nL, stride=None, overlap=1):
+        repetitive_mask = \
+            LeftRepetitiveMask.get_mask(nL, stride=stride, overlap=overlap)
+        repetitive_mask = SubsequentMask.get_mask(nL) * repetitive_mask
+        return repetitive_mask
+
+
+class RightRepetitiveMask(SparseMask):
+    @classmethod
+    @allow_non_square
+    @compute_stride
+    @numpy
+    def get_indices(self, nI, nO, stride=None, overlap=1, **kwargs):
+        indices = []
+        row_indices = np.arange(0, nI)
+        indxs = (row_indices % stride) >= (stride - overlap)
+        col_indices = row_indices[indxs]
+        col_indices = col_indices - (stride - overlap)
+
+        for row in np.arange(0, nO):
+            if nI == nO:
+                indices.append([row, row])
+            for col in col_indices:
+                indices.append([row, col])
+        return indices
+
+
+class CircleMask(SparseMask):
+    @classmethod
+    @numpy
+    def get_indices(self, nΙ, nO, *args, **kwargs):
+        for i in range(nO):
+            # left neighbor
+            if i == 0:
+                indices.append([i, nI - 1])
+            else:
+                indices.append([i, i - 1])
+
+            # right neighbor
+            if i != last_indx:
+                indices.append([i, i + 1])
+            else:
+                indices.append([i, 0])
+        return indices
+
+
+class CircleGridMask(SparseMask):
+    def get_neighbors(i, j, nO, nI):
+        neighbors = []
+        if i == 0:
+            # up neighbor folds
+            neighbors.append([nO - 1, j])
+        else:
+            neighbors.append([i - 1, j])
+        if j == 0:
+            # left neighbor folds
+            neighbors.append([i, nI - 1])
+        else:
+            neighbors.append([i, j - 1])
+
+        if i == nO - 1:
+            # down neighbor folds
+            neighbors.append([0, j])
+        else:
+            neighbors.append([i + 1, j])
+
+        if j == nI - 1:
+            # right neighbor folds
+            neighbors.append([i, 0])
+        else:
+            neighbors.append([i, j + 1])
+        return neighbors
+
+    @classmethod
+    @allow_non_square
+    @numpy
+    def get_indices(self, gridI, gridO, **kwargs):
+        '''
+            gridI: (rowsI, colsI)
+            gridO: (rowsO, colsO)
+        '''
+        rowsI, colsI = gridI
+        rowsO, colsO = gridO
+        indices = []
+        for i in range(rowsO):
+            for j in range(colsO):
+                neighbors = self.get_neighbors(i, j, rowsI, colsI)
+                x_index = self.convert_to_1d(i, j, colsO)
+                indices += [[x_index, self.convert_to_1d(*x, colsI)] for x in neighbors]
+            # if rowsI == rowsO and colsI == colsO:
+            #     indices.append([x_index, x_index])
+        return indices
+
+
+class DirectRelayMask(SparseMask):
+    @classmethod
+    @numpy
+    def get_indices(self, nL, **kwargs):
+        indices = []
+        for i in range(nL - 1):
+            indices.append([nL - 1, i])
+        return indices
+
+
+class ReverseRelayMask(SparseMask):
+    @classmethod
+    @numpy
+    def get_indices(self, nL, **kwargs):
+        indices = []
+        for i in range(nL - 1):
+            indices.append([i, nL - 1])
+        return indices
+
+
+class ConstantDegreeRandomMask(SparseMask):
+    @classmethod
+    @numpy
+    def get_indices(self, nI, nO, deg, **kwargs):
+        # send edges from nI to nO
+        # indices containts tuples with max: [nO, nI]
+        indices = []
+        for i in range(nI):
+            for _ in range(deg):
+                # find a random neighbor
+                j = random.randint(0, nO - 1)
+                indices.append([j, i])
+        return indices
+
+
+class SlidingMask(SparseMask):
+    @classmethod
+    @allow_non_square
+    @numpy
+    def get_indices(self, nI, nO, overlap=1, **kwargs):
+        deg = math.ceil(((nI + (nO - 1) * overlap) / nO))
+        indices = []
+        start = 0
+        for i in range(nO):
+            for k in range(deg):
+                y_index = k + start
+                if y_index >= nI:
+                    y_index = nI - y_index
+                indices.append([i, y_index])
+            start += deg - 1
+            if start >= nI:
+                start = nI - start
+        return indices
 
 
 class MasksCollection:
